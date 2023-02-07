0ed890a19e6a259247bbb0d3f689540e917b264c
Morella92 <morena.piem@outlook.com>
2023-02-07 19:44:52 +0100

    FIX passaggi

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-console.log('While vs For')
+console.log('While vs for')
 
 // Utilizziamo un ciclo per chiedere all’utente un numero tramite prompt.
 
@@ -6,40 +6,28 @@ let odds = []
 console.log(odds)
 
 let userNumber 
-console.log(userNumber)
 
 // Continuiamo a chiedere un numero all’utente finchè non avremo ottenuto 10 numeri dispari. 
 
-do{
+while (odds.length < [10]) {
 
-    userNumber = parseInt (prompt('Inserisci un numero'))
-}
-
-while (userNumber < 10) {
+    userNumber = prompt('Inserisci un numero')
 
     // Tutti gli altri valori li scartiamo (numeri pari ed eventuali NaN).
-
-    if((isNaN(userNumber)) || (userNumber % 2 === 0)){
+    if ((!isNaN(userNumber)) && (userNumber % 2 != 0)) {
 
         // Se il numero è dispari, lo salviamo in una lista.
 
         odds.push(userNumber)
 
-        userNumber++
     }
-
 }
+// console.log(odds)
 
 // Una volta ottenuti tutti i 10 numeri, stampiamoli in console uno alla volta
 
-for(let i = 0; i < odds.length; i++) {
+for (let i = 0; i < odds.length; i++) {
 
     console.log(odds[i])
 }
-
-
-
-
-
-
 
