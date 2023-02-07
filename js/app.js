462b9cb7827a8b0e614eea38969d41947730d643
Morella92 <morena.piem@outlook.com>
2023-02-07 16:03:04 +0100

    FEAT ciclo for

--- js/app.js
+++ js/app.js
@@ -1,32 +1,41 @@
 console.log('While vs For')
+
 // Utilizziamo un ciclo per chiedere all’utente un numero tramite prompt.
 
-let odd = []
-console.log(odd)
+let odds = []
+console.log(odds)
 
 let userNumber 
 console.log(userNumber)
 
-// Se il numero è dispari, lo salviamo in una lista.
-
-if(userNumber % 2 !== 0){
-    odd.push(userNumber)
-}
-
 // Continuiamo a chiedere un numero all’utente finchè non avremo ottenuto 10 numeri dispari. 
 
 do{
+
     userNumber = parseInt (prompt('Inserisci un numero'))
+}
+
+while (userNumber < 10) {
 
     // Tutti gli altri valori li scartiamo (numeri pari ed eventuali NaN).
-} while ((isNaN(userNumber)) || (userNumber % 2 === 0)) {
 
-    
+    if((isNaN(userNumber)) || (userNumber % 2 === 0)){
+
+        // Se il numero è dispari, lo salviamo in una lista.
+
+        odds.push(userNumber)
+
+        userNumber++
+    }
 
 }
 
-
 // Una volta ottenuti tutti i 10 numeri, stampiamoli in console uno alla volta
+
+for(let i = 0; i < odds.length; i++) {
+
+    console.log(odds[i])
+}
 
 
 
